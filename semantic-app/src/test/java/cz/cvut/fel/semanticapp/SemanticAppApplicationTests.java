f9e82015b4e006134b44ae853238449e8234274d
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2026-03-04 09:33:21 +0100

    Restructure project, remove the example application.

--- semantic-app/src/test/java/cz/cvut/fel/semanticapp/SemanticAppApplicationTests.java
+++
@@ -1,8 +0,0 @@
-package cz.cvut.fel.semanticapp;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SemanticAppApplicationTests {
-}
