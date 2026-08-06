cc46e02eacc9bb777f15b7b138a4af3182a5a6c8
Lukáš Kaňka <lukaskabc@gmail.com>
2026-08-06 10:37:41 +0200

    [Fix] Update schema to validate addResource and addProperty required properties

--- src/main/java/cz/cvut/kbss/exception/MigrationExecutionException.java
+++ src/main/java/cz/cvut/kbss/exception/MigrationExecutionException.java
@@ -4,4 +4,8 @@ public class MigrationExecutionException extends OntologyMigrationToolException 
     public MigrationExecutionException(String message, Throwable cause) {
         super(message, cause);
     }
+
+    public MigrationExecutionException(String message) {
+        super(message);
+    }
 }
